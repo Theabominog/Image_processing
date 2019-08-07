bcfb2b1a6cbcce8d45c2cbd1e55fd318417d1f2f
Theabominog <40739610+Theabominog@users.noreply.github.com>
2019-08-07 23:06:59 +0530

    Add files via upload

--- Task1_code.m
+++ Task1_code.m
@@ -1,11 +1,11 @@
 n = 10000;
 for i = 0:100
-x(i+1,1) = i/100;
-if i<=50
-ux(i+1,1) = i/100;
-else
-ux(i+1,1) = 1- (i/100);
-end
+    x(i+1,1) = i/100;
+    if i<=50
+        ux(i+1,1) = i/100;
+    else
+        ux(i+1,1) = 1- (i/100);
+    end
 end
 
 u0=ux;
@@ -13,20 +13,20 @@ u1(1,1) = u0(1,1);
 u1(101,1) = u0(101,1);
 
 for i = 1:n
-for j = 2:100
-u1(j,1)=u0(j,1)+0.5*(u0(j+1,1)-2*u0(j,1)+u0(j-1,1));
-end
-u0 = u1;
-if i==10
-plot(x,u1)
-hold on
-elseif i==100
-plot(x,u1);
-elseif i==1000
-plot(x,u1);
-elseif i==5000
-plot(x,u1);
-end
+    for j = 2:100
+        u1(j,1)=u0(j,1)+0.5*(u0(j+1,1)-2*u0(j,1)+u0(j-1,1));
+    end
+    u0 = u1;
+    if i==10
+        plot(x,u1)
+        hold on
+    elseif i==100
+        plot(x,u1);
+    elseif i==1000
+        plot(x,u1);
+    elseif i==5000
+        plot(x,u1);
+    end
 end
 
 plot(x,u1)
