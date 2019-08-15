d2efdf36cf4cf1cdd43112d4f7172c0e1bf19ff8
Nexus <53861427+Nexus-404@users.noreply.github.com>
2019-08-15 16:50:59 +0530

    Create Task8_code.m

---
+++ Task8_code.m
@@ -0,0 +1,47 @@
+clear;
+
+u0 = imread('test2.png');
+abc = u0;
+u0 = im2double(u0);
+u0 = imnoise(u0,'salt & pepper',0.1);
+[m,n] = size(u0);
+
+u = u0;
+u1 = u;
+
+for i = 2:m -1
+    for j = 2:n -1
+
+        g(i,j) = task8_func(u(i,j));
+
+    end
+end
+
+ for t = 1:10;
+    for i = 2:m -2
+        for j = 2:n -2
+
+            G(i,j) = g(i,j)/4;
+
+        end
+    end
+
+    for i = 2:m -2
+        for j = 2:n -2
+
+            u1(i,j) = u(i,j) + ((G(i,j)*(u(i+1,j)-u(i,j))) - (G(i,j)*(u(i,j)-u(i-1,j))) + (G(i,j)*(u(i,j+1)-u(i,j)))-(G(i,j)*(u(i,j)-u(i,j-1))));
+
+        end
+        u = u1;
+    end
+end
+
+figure
+subplot(1,4,1),
+imshow(u0),title('Noised(S&P)');
+subplot(1,4,2),
+imshow(u),title('by PDE');
+subplot(1,4,3),
+imshow(abc),title('Actual Photo');
+subplot(1,4,4),
+imshow(medfilt3(u0)),title('Using medfilt3');
